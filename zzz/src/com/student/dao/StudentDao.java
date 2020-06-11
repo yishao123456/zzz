12d0c2a42af14b457e26c35e415c5f47bd0e09b8
yishao123456 <1043812440@qq.com>
2020-06-11 09:20:32 +0800

    提交到本次仓库

---
+++ zzz/src/com/student/dao/StudentDao.java
@@ -0,0 +1,5 @@
+package com.student.dao;
+
+public class StudentDao {
+
+}
---
+++ zzz/src/com/student/model/StudentModel.java
@@ -0,0 +1,41 @@
+package com.student.model;
+
+public class StudentModel {
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public String getLove() {
+		return love;
+	}
+	public void setLove(String love) {
+		this.love = love;
+	}
+	
+
+}
